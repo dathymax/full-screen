a5d40370c54225f103c5aebb1b5818c2633cd882
Do Tuan Dat <75316700+dathymax@users.noreply.github.com>
2021-12-15 14:21:22 +0700

    cap nhat

--- src/components/common/header/BreadCrumb.js
+++ src/components/common/header/BreadCrumb.js
@@ -1,50 +1,40 @@
-import { Breadcrumb } from 'antd';
-import React from 'react'
-import { useLocation, Link } from 'react-router-dom';
+import React from "react";
+import { useLocation, Link } from "react-router-dom";
+import { Breadcrumb } from "antd";
 
-const HeaderBreadCrumb = () => {
-    const location = useLocation()
-    const breadcrumbView = () => {
-        const { pathname } = location;
-        const pathnames = pathname.split('/').filter((item) => item)
-        return (
-            <div>
-                <Breadcrumb>
-                    {pathnames.length > 0 ? (
-                        <Breadcrumb.Item>
-                            <Link to="/">
-                                Home
-                            </Link>
-                        </Breadcrumb.Item>
-                    ) : (
-                            <Breadcrumb.Item>
-                                Home
-                            </Breadcrumb.Item>
-                    )}
-                    {pathnames.map((name, index) => {
-                        const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`
-                        const isLast = index === pathnames.length - 1
-                        return isLast ? (
-                            <Breadcrumb.Item>
-                                {name}
-                            </Breadcrumb.Item>
-                        ) : (
-                                <Breadcrumb.Item>
-                                    <Link to={`${routeTo}`}>
-                                        {name}
-                                    </Link>
-                                </Breadcrumb.Item>
-                        )
-                    })}
-                </Breadcrumb>
-            </div>
-        )
-    }
+const BreadCrumb = () => {
+  const location = useLocation();
+  const breadCrumbView = () => {
+    const { pathname } = location;
+    const pathnames = pathname.split("/").filter((item) => item);
+    const capatilize = (s) => s.charAt(0).toUpperCase() + s.slice(1);
     return (
-        <div className="breadcrumb">
-            {breadcrumbView()}
-        </div>
-    )
-}
+      <div>
+        <Breadcrumb>
+          {pathnames.length > 0 ? (
+            <Breadcrumb.Item>
+              <Link to="/">Home</Link>
+            </Breadcrumb.Item>
+          ) : (
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+          )}
+          {pathnames.map((name, index) => {
+            const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
+            const isLast = index === pathnames.length - 1;
+            return isLast ? (
+              <Breadcrumb.Item>{capatilize(name)}</Breadcrumb.Item>
+            ) : (
+              <Breadcrumb.Item>
+                <Link to={`${routeTo}`}>{capatilize(name)}</Link>
+              </Breadcrumb.Item>
+            );
+          })}
+        </Breadcrumb>
+      </div>
+    );
+  };
 
-export default HeaderBreadCrumb
+  return <>{breadCrumbView()}</>;
+};
+
+export default BreadCrumb;
